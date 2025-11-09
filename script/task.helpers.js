23c0c1d13397d5d899f7c01ace51a2fe60bd35ba
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-11-09 09:15:33 +0800

    added a function for filtering complete task and changing it from localstorage

--- script/task.helpers.js
+++ script/task.helpers.js
@@ -1,7 +1,4 @@
-import modalManager, {
-   openModal as open,
-   closeModal as close,
-} from "./modal.manager.js";
+import modalManager, { openModal as open } from "./modal.manager.js";
 
 export function setID() {
    let current_id = JSON.parse(localStorage.getItem("id")) || 1;
@@ -21,13 +18,16 @@ export function processDate(date) {
 }
 
 export function editData(...newValue) {
+   debugger
    let [parent, newTask, newDate] = newValue;
    const data = parent.querySelector(".todo__info");
-   const dataId = data.dataset.id;
+   const dataId = parseInt(data.dataset.id)
 
    let storage = JSON.parse(localStorage.getItem("storage")).map(
       (currentTask) => {
-         if (currentTask.id == dataId) {
+         
+         if (currentTask.id === dataId) {
+
             return {
                id: currentTask.id,
                task: newTask,
@@ -35,39 +35,45 @@ export function editData(...newValue) {
                isComplete: currentTask.isComplete,
             };
          }
-         return currentTask
+         return currentTask;
       }
    );
 
-   document.querySelector(".main__todo__container").innerHTML = "";
-   localStorage.setItem("storage", JSON.stringify(storage))
+   localStorage.setItem("storage", JSON.stringify(storage));
    renderTask();
 }
 
 export function renderTask() {
+   modalManager.todoContainer.innerHTML = "";
    let storage = JSON.parse(localStorage.getItem("storage"));
-   if (!storage || storage == "") return;
+   if (!storage || storage.length === 0) return;
 
    storage.forEach((task) => {
+      // create main todo container
       let el = document.createElement("div");
-      el.setAttribute("class", `todo todo__${task.id}`);
+      el.classList.add("todo", `todo__id-${task.id}`);
+      if (task.isComplete) el.classList.add("done");
       el.innerHTML = `
-         <div class="todo__info" data-id=${task.id}>
+         <div class="todo__info" data-id="${task.id}">
             <div class="todo__info__left">
-               <input type="radio" id="todo__radio__${task.id}" value="${task.id}" />
-               <span id="todo__task"> ${task.task} </span>
+               <input type="checkbox" id="todo__checkbox__${task.id}" value="${task.id}" ${task.isComplete ? "checked" : ""}/>
+               <span id="todo__task">${task.task}</span>
             </div>
             <div class="todo__info__right">
                <div class="todo__info__btns">
-                  <img src="img/edit_calendar_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" dataset-id="${task.id}" class="edit__btn"/>
-                  <img src="img/delete_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" dataset-id="${task.id}" class="dlt__btn" />
+                  <img src="img/edit_calendar_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" data-id="${
+                     task.id
+                  }" class="edit__btn"/>
+                  <img src="img/delete_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" data-id="${
+                     task.id
+                  }" class="dlt__btn"/>
                </div>
-               <span id="todo__date"> ${task.deadline} </span>
+               <span id="todo__date">${task.deadline}</span>
             </div>
          </div> 
-         `;
+      `;
 
-      document.querySelector(".main__todo__container").append(el);
+      modalManager.todoContainer.append(el);
       attachListeners(task);
    });
 }
@@ -75,9 +81,10 @@ export function renderTask() {
 // modularize
 
 function attachListeners(task) {
-   let parent = document.querySelector(`.todo__${task.id}`);
+   let parent = document.querySelector(`.todo__id-${task.id}`);
    parent.querySelector("input").addEventListener("click", (e) => {
-      parent.querySelector("span").classList.toggle("done");
+      // we can check and uncheck
+      checkTask(e.target, task, filterCompletedTask);
    });
 
    let btns = parent.querySelector(".todo__info__btns");
@@ -104,4 +111,37 @@ function attachListeners(task) {
          parent.remove();
       });
    }
+}
+
+function checkTask(checkbox, task, filter) {
+   let storage = JSON.parse(localStorage.getItem("storage"));
+
+   storage.forEach((currentTask) => {
+      if (currentTask.id === task.id) {
+         currentTask.isComplete = checkbox.checked; // true or false
+      }
+   });
+
+   localStorage.setItem("storage", JSON.stringify(storage));
+
+   // visually toggle 'done' class
+   let parent = document.querySelector(`.todo__id-${task.id}`);
+   if (checkbox.checked) {
+      parent.classList.add("done");
+   } else {
+      parent.classList.remove("done");
+   }
+
+   filter();
+}
+
+function filterCompletedTask() {
+   let storage = JSON.parse(localStorage.getItem("storage"));
+   let [completedTask, notCompleted] = [
+      storage.filter((currentTask) => currentTask.isComplete == true),
+      storage.filter((currentTask) => currentTask.isComplete == false),
+   ];
+
+   localStorage.setItem("storage", JSON.stringify(notCompleted.concat(completedTask)));
+   renderTask();
 }
