270aa04bb332c2a57223a9af0cf8f6ed428bc20c
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-19 20:39:58 +0800

    add: To-Do class

---
+++ src/todo/Todo.js
@@ -0,0 +1,25 @@
+import Helper from "./Todo.helper.js";
+
+export default class Todo {
+   
+   constructor(name, category, description, due, status, checkList, tags, priority) {
+      this._id = crypto.randomUUID();
+      this._name = name
+      this._category = category
+      this._description = Helper.setDesc(description)
+      this._due = Helper.setDue(due)
+      this._status = status
+      this._checkList = Helper.setCheckList(checkList)
+      this._tags = Helper.setTags(tags)
+      this._priority = priority
+   }
+
+   get name() { return this._name }
+   get category() { return this._category }
+   get description() { return this._description}
+   get due() { return this._due}
+   get status() { return this._status}
+   get checkList() { return this._checkList}
+   get tags() { return this._tags}
+   get priority() { return this._priority}
+}
