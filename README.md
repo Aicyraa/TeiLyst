eddcb36cb61f96813557716158794c885d77c437
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-19 18:19:18 +0800

    remove unused files and code for a cleaner project structure

--- README.md
+++
@@ -1,8 +0,0 @@
-# To-do-List
-
-- User can add task
-- The task has a date of creation and edit
-- The task can be edited or deleted
-- The System is responsinve
-- THe system can bookmark in mobile
-
--- script/task.class.js
+++
@@ -1,8 +0,0 @@
-export default class Task{
-   constructor(id, description, deadline) {
-      this.id = id
-      this.task = description
-      this.deadline = deadline
-      this.isComplete = false
-   }
-}
