cb4263c7eff5d7a9591f07e43edac74fe4a37fd4
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-20 19:34:44 +0800

    test: project filter test

--- src/script.js
+++ src/script.js
@@ -3,8 +3,10 @@ import Project from "./project/project.js";
 import { countTodo, getAllCategory, getAllTags } from "./todo/utilities.js";
 import { FDue, FDueBefore, FDueAfter, FDueRange } from "./utilities/filter.js";
 import { STodo, SCategory, SDue } from "./utilities/sort.js";
+import { switchProject, renderTodo } from "./ui/utilities.js";
 
-const todos = [
+
+const todos_1 = [
    new Todo(null, "Buy groceries", "Personal", "Pick up fruits, vegetables, and milk from the store", "2026-02-25T14:30", "pending", "Fruits,Vegetables,Milk", "shopping,errands", "high"),
    new Todo(null, "Fix login bug", "Work", "Users are getting 401 errors after token refresh", "2026-02-21T09:00", "in-progress", "Reproduce issue,Check token logic,Write tests", "bug,auth", "high"),
    new Todo(null, "Read Clean Code", "Learning", "Finish chapaters 5 through 8", "2026-03-01T18:00", "pending", "Chapter 5,Chapter 6,Chapter 7,Chapter 8", "books,self-improvement", "low"),
@@ -13,6 +15,15 @@ const todos = [
    new Todo(null, "Learn JS", "Career", "Yea", "2026-03-10T10:00", "In-progress", "Add projects,Update about,Deploy", "Programming", "medium"),
 ];
 
+switchProject("House");
+
+const todos_2 = [
+   new Todo(null, "Test Test", "Personal", "Pick up fruits, vegetables, and milk from the store", "2026-02-25T14:30", "pending", "Fruits,Vegetables,Milk", "shopping,errands", "high"),
+   new Todo(null, "Test", "Work", "Users are getting 401 errors after token refresh", "2026-02-21T09:00", "in-progress", "Reproduce issue,Check token logic,Write tests", "bug,auth", "high"),
+   new Todo(null, "Test", "Learning", "Finish chapaters 5 through 8", "2026-03-01T18:00", "pending", "Chapter 5,Chapter 6,Chapter 7,Chapter 8", "books,self-improvement", "low"),
+];
+
+
 const projects = [
    new Project(null, "Math", "All about math"),
    new Project(1, "Code", "All about code"),
@@ -20,13 +31,8 @@ const projects = [
    new Project(2, "House", "All about house"),
 ]
 
-function switchProject(project) {
-   // temp only
-   localStorage.setItem("teilist-active-project")
-}
-
-function renderTodo(todo) {
-   // temp only
-}
-
-console.log(projects);
+console.log(renderTodo(todos_1.concat(todos_2)))
+switchProject("Code")
+console.log(renderTodo(todos_1.concat(todos_2)))
+switchProject("All")
+console.log(renderTodo(todos_1.concat(todos_2)))
