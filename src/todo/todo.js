7d9ffce52ab9a8aa4e4eed6ad5342abc9b135f74
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-20 16:25:45 +0800

    fix: property get for due

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,9 +1,11 @@
+import { format } from "date-fns";
 import Helper from "./helper.js";
 
 export default class Todo {
-   
-   constructor(name, category, description, due, status, checkList, tags, priority) {
+
+   constructor(background, name, category, description, due, status, checkList, tags, priority) {
       this._id = crypto.randomUUID();
+      this._background = 'linear-gradient(120deg, lightgreen, lightblue)'
       this._name = name
       this._category = category
       this._description = Helper.setDesc(description)
@@ -18,6 +20,7 @@ export default class Todo {
    get category() { return this._category }
    get description() { return this._description}
    get due() { return this._due}
+   get dueFormatted() { return format(this._due, "MMM dd, yyyy 'at' h:mm a") }
    get status() { return this._status}
    get checkList() { return this._checkList}
    get tags() { return this._tags}
